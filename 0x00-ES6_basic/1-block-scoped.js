7d3ae505f4e6e05770847403b326cb425a4b0618
Pebong2023 <ebongchris21@gmail.com>
2024-03-04 23:14:00 +0000

    Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+
+rt default function taskBlock(trueOrFalse) {
+	  var task = false;
+	  var task2 = true;
+
+	  if (trueOrFalse) {
+		       task;
+		       task2;
+		    }
+
+	  return [task, task2];
+}
