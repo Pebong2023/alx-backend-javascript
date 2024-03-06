98d425bcbf3ef7f591e77812d610b9f2c934f333
Pebong2023 <ebongchris21@gmail.com>
2024-03-06 21:21:18 +0000

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+export default function getResponseFromAPI() {
+  const myPromise = new Promise((resolve, reject) => {
+    // Simulating API response after a delay
+    setTimeout(() => {
+      // Simulate a successful API response
+      const responseData = { message: "API response received" };
+      resolve(responseData);
+
+    }, 2000); // Simulate a delay of 2 seconds
+  });
+
+  return myPromise;
