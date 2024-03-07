4330833fffb2f2d479f94d98deab8039fa9ebeed
Pebong2023 <ebongchris21@gmail.com>
2024-03-07 15:39:35 +0000

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
-export default function ngetResponseFromAPI() {
-const myPromise = new Promise(resolve, reject) => {
-//Simulating API response after a delay
-setTimeout(() => {
-// Simulating a succeeful API response
-const responseData = {message: "API response recieved" };
-resolve(responseData);
-
-}, 200); // Simulate a delay of 2 seconds
-});
-
-return MyPromise;
+export default function getResponseFromAPI() {
+  return Promise.resolve();
 }
