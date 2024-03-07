85a6509a4bc37b61e739e741530df2f95158529a
Pebong2023 <ebongchris21@gmail.com>
2024-03-07 11:50:27 +0000

    A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const classrooms = [];
+  for (const i of sizes) {
+    classrooms.push(new ClassRoom(i));
+  }
+  return classrooms;
+}
