77503dc90a0ea4b422fb603d423b51670c1f30e1
Pebong2023 <ebongchris21@gmail.com>
2024-03-07 22:01:19 +0000

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,18 +1,15 @@
-const brandSymbol = Symbol('brand');
-const motorSymbol = Symbol('motor');
-const colorSymbol = Symbol('color');
-
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
-    this[brandSymbol] = brand;
-    this[motorSymbol] = motor;
-    this[colorSymbol] = color;
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this.prototype.constructor;
   }
 
   cloneCar() {
-    const { [brandSymbol]: brand, [motorSymbol]: motor, [colorSymbol]: color } = this;
-    return new Car(brand, motor, color);
+    return new (this.constructor[Symbol.species])();
   }
 }
-
-export default Car;
